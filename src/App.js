c01f2aff11337c5aac5b7c214ff90ce6eeaa6ba6
Vishnuu CV <vishnuucv@Vishnuus-MacBook-Pro.local>
2022-09-10 08:23:38 +0530

    Home page file created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignIn from "./components/signIn/SignIn";
 import SignUp from "./components/signup/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         {/* <Route path="/" element={<Home />} />       */}
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
